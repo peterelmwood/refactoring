655e33a42963a2b54f55bb2bd0e6de916909a7ca
peterelmwood <peterelmwood@gmail.com>
2020-11-22 12:09:19 -0600

    reformat totalVolumeCredits

--- first_example/totalVolumeCredits.js
+++ first_example/totalVolumeCredits.js
@@ -1,4 +1,4 @@
-function totalVolumeCredits(invoice) {
+function totalVolumeCredits() {
   let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
