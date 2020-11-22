03ad4cec871409c756eb0ac3ca13fdeb3c15698d
peterelmwood <peterelmwood@gmail.com>
2020-11-22 12:50:34 -0600

    slide `totalAmount` statement next to accumulator

--- first_example/statement.js
+++ first_example/statement.js
@@ -1,6 +1,6 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
 
     // print line for this order
