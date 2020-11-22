9a482414cf06d56e324c7ca6b14da7957a517b01
peterelmwood <peterelmwood@gmail.com>
2020-11-22 12:30:04 -0600

    extract `playFor` function

--- first_example/statement.js
+++ first_example/statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -24,6 +24,10 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   function amountFor(aPerformance , play) {
     let result = 0;
