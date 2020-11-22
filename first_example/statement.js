ec093a60e225ac6154dac57de7699ff04cc84e54
peterelmwood <peterelmwood@gmail.com>
2020-11-22 13:05:53 -0600

    split phase to separate phases of calculation
    divide the logic into two parts:
    
    one that calculates the data required for the statement
    
    the other that renders it into text or HTML

--- first_example/statement.js
+++ first_example/statement.js
@@ -1,5 +1,12 @@
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
+  for (let perf of invoice.performances) {
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
