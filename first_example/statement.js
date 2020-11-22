3db9b809ba27757e921b6a9e8013026117a8b06d
peterelmwood <peterelmwood@gmail.com>
2020-11-22 11:45:48 -0600

    removing play variable

--- first_example/statement.js
+++ first_example/statement.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
     }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
