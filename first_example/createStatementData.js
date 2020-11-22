1c55a7add0a9d46d5b5ca7d64854473ea419a5c4
peterelmwood <peterelmwood@gmail.com>
2020-11-22 14:17:45 -0600

    introduce additional functionality now that code is well-factored

--- first_example/createStatementData.js
+++ first_example/createStatementData.js
@@ -1,4 +1,4 @@
-function createStatementData(invoice, plays) {
+export default function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
